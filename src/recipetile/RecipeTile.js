b7e06629510e1d70449685851bd6b0471e44fdb8
shsandadi <harsha.sandadi@gmail.com>
2021-12-15 23:35:58 +0530

    Add external url on clicking of Image

--- src/recipetile/RecipeTile.js
+++ src/recipetile/RecipeTile.js
@@ -1,11 +1,20 @@
 import React from "react";
-import "./style.css"
+import "./style.css";
 function RecipeTile({ recipe }) {
   return (
-    <div className="recipeTile">
-      <img className="recipeTile__image" src={recipe["recipe"]["image"]} alt="tile-image" />
-      <p className="recipeTile__name">{recipe["recipe"]["label"]}</p>
-    </div>
+    recipe["recipe"]["image"].match(/\.(jpeg||jpg|gif|png)$/) != null && (
+      <div className="recipeTile">
+        <img
+          className="recipeTile__image"
+          src={recipe["recipe"]["image"]}
+          alt="tile-image"
+          onClick={()=>window.open(recipe["recipe"]["url"])
+
+          }
+        />
+        <p className="recipeTile__name">{recipe["recipe"]["label"]}</p>
+      </div>
+    )
   );
 }
 
